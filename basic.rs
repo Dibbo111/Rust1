1820413d62c28bc64de356276c947cff5894a579
Dibbo111 <mahdikohee@gmail.com>
2025-07-14 20:03:40 +0600

    Create basic.rs

---
+++ basic.rs
@@ -0,0 +1,21 @@
+use std::fs::File;
+use std::io::Write;
+use std::process::exit;
+fn main() {
+    // /dev/stdout হলো Unix / linux  এর standard output device
+    let mut stdout = match File::create("/dev/stdout") {
+        Ok(f) => f,
+        Err(e) => {
+            eprintln!("Error opening /dev/stdout: {}", e);
+            exit(1);
+        }
+    };
+    let message = b"Hacker Hello, World!\n";
+    match stdout.write_all(message) {
+        Ok(_) => {},
+        Err(e) => {
+            eprintln!("Error writing to stdout: {}", e);
+            exit(1);
+        }
+    };
+}
