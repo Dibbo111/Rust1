3edaf11339b7d5b6386f11d90b2e4c0871a3b148
Dibbo111 <mahdikohee@gmail.com>
2025-07-16 16:15:58 +0600

    Update basic.rs

--- basic.rs
+++ basic.rs
@@ -41,46 +41,30 @@ fn main(){
     std::io::Write::flush(&mut stdout).expect("Failed !") ;         //remember that this line must return error 
 }
 
-//another simple example of writing hello world 
-use std::io;
-use std::io::Write;
-fn main() -> Result<(), io::Error> {
-    let message: &[u8] = b"Hello World!\n";
-    let stdout: io::Stdout = io::stdout();
-    let mut handle: io::StdoutLock = stdout.lock();
-    let mut i: usize = 0;
-    let message_len: usize = message.len();
-    while i < message_len {
-        let start_index: usize = i;
-        let end_index: usize = i + 1;
-        let slice: &[u8] = &message[start_index..end_index];
-        let mut total_written: usize = 0;
-        let slice_len: usize = slice.len();
-        while total_written < slice_len {
-            let write_result: Result<usize, io::Error> = handle.write(&slice[total_written..slice_len]);
-            match write_result {
-                Ok(bytes_written) => {
-                    if bytes_written == 0 {
-                        let error: io::Error = io::Error::new(io::ErrorKind::WriteZero, "write returned 0 bytes");
-                        return Err(error);
-                    }
-                    total_written = total_written + bytes_written;
-                },
-                Err(error) => {
-                    return Err(error);
-                }
-            }
-        }
-        i = i + 1;
-    }
-    let flush_result: Result<(), io::Error> = handle.flush();
-    match flush_result {
-        Ok(()) => {
-            return Ok(());
-        },
+------------------------------------------------------------------hello world end --------------------------------
+_________________________________________________str____________________________________________________________
+//all of string in rust 
+fn main(){
+    let message : &'static str = "Hello world" ;
+    std::println!("{:?}" , message) ;
+    std::process::exit(1) ;
+    //it will never execute !
+}
+
+//another simple example 
+fn main(){
+    let mut stdout : std::io::Stdout = std::io::stdout() ;
+    let message : &'static str = "Hello world and Dibbo !" ;
+    match std::io::Write::write_all(&mut stdout , message.as_bytes()){
+        Ok(done) => done ,
         Err(error) => {
-            return Err(error);
+            let mut stderr : std::io::Stderr = std::io::stderr() ;
+            let err_msg : String = format!("Error as {:?}" , error) ;
+            std::io::Write::write_all(&mut stderr , err_msg.as_bytes()).unwrap() ;
+            std::process::exit(1) ;
         }
     }
 }
-_____________________________________________hello world done_____________________________________________________
+
+//
+
